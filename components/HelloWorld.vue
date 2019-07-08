8c7ac1d7ad60ba4b7e537296bcb5de5f843d0b54
Liu <1056474831@qq.com>
2019-07-08 09:57:38 +0800

    first commit

---
+++ components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{Home}}</div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  watch: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+<style>
+</style>
